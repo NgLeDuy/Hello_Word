ea08526b877897270eb003e040c763164c9e8fb5
NgLeDuy <52234363+NgLeDuy@users.noreply.github.com>
2019-07-19 02:23:12 -0700

    Add files via upload

---
+++ hello_world.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express();
+
+
+app.get('/', (req, res)=> 
+{
+    res.send("Hello Woorld");
+});
+
+console.log("Hello World");
+
+app.set('port', process.env.PORT || 3000);
+app.listen(app.get('port'), ()=>{
+    console.log('Express start on http://localhost:3000')
+});
